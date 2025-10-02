7dd578e22aac07c1587ae0925fb8008c2d0d356c
beingzuhairkhan <zuhairkhan5134@gmail.com>
2025-10-02 18:02:09 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,18 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  // getHello(): string {
-  //   let a = 40 ;
-  //   let b = 20 ;
-  //   if(a > b){
-  //     console.log(a)
-  //     return `Hello world ${a} is greater `
-  //   }else{
-  //     return `Hello world ${b} is greater `
-  //   }
-
-  //   // return 'Hello World! zuhair khan';
-  // }
 
   // getBye():string{
   //    return 'Hello World! zuhair khan';
